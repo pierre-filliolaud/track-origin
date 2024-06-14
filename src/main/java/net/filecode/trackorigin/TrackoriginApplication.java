1c64a78423f951c6d219e0f5c4a7bfbe31203827
Pierre Filliolaud <git-dev@filliolaud.net>
2024-06-14 21:34:21 +0200

    intial revision

---
+++ src/main/java/net/filecode/trackorigin/TrackoriginApplication.java
@@ -0,0 +1,13 @@
+package net.filecode.trackorigin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackoriginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackoriginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/filecode/trackorigin/TrackoriginApplicationTests.java
@@ -0,0 +1,13 @@
+package net.filecode.trackorigin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackoriginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
